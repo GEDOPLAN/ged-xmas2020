d5ca05926a7678149876c40f376bf9f776a1a2be
Dominik Mathmann <dominik.mathmann@gedoplan.de>
2020-12-08 11:24:05 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ged-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'gedoplan-angular-xmas2020';
+}
